ad7d08d1d361a74f9d144ade5467ad45cb6a0157
Stuart Erskine <stuart.marr.erskine@gmail.com>
2024-03-27 23:05:05 -0300

    Working jar file execution.

--- src/main/java/launcher/ArcadeLauncher.java
+++ src/main/java/launcher/ArcadeLauncher.java
@@ -4,6 +4,8 @@ import launcher.framework.util.Logger;
 import launcher.menu.MenuGame;
 
 import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class ArcadeLauncher {
     public static final String PROP_LIBRARY_PATH = "net.java.games.input.librarypath";
@@ -11,13 +13,7 @@ public class ArcadeLauncher {
 
         Logger.info(ArcadeLauncher.class.getSimpleName() + " started.");
 
-        final String libraryPath = new File("src/main/resources/lib").getAbsolutePath();
-
-//        System.setProperty(PROP_LIBRARY_PATH, libraryPath);
-
-        System.load(libraryPath + File.separator + "jinput-dx8_64.dll");
-        System.load(libraryPath + File.separator + "jinput-raw_64.dll");
-
+        loadLibraries();
         final Thread gameThread = new Thread(new MenuGame());
         gameThread.start();
 
@@ -28,5 +24,45 @@ public class ArcadeLauncher {
         }
 
         Logger.info(ArcadeLauncher.class.getSimpleName() + " complete.");
+    }
+
+    private static void loadLibraries() {
+        try {
+            final String[] nativeLibraries = {"jinput-dx8_64.dll", "jinput-raw_64.dll"};
+
+            for (String library : nativeLibraries) {
+                loadNativeLibrary(library);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load libraries.", e);
+        }
+    }
+
+    private static void loadNativeLibrary(String library) throws IOException {
+        // Get the input stream for the native library
+        InputStream inputStream = ArcadeLauncher.class.getResourceAsStream("/lib/" + library);
+        if (inputStream == null) {
+            throw new FileNotFoundException("Native library " + library + " not found in resources.");
+        }
+
+        // Create a temporary file to copy the native library
+        File tempFile = File.createTempFile(library, null);
+        tempFile.deleteOnExit();
+
+        // Copy the native library from the input stream to the temporary file
+        try (FileOutputStream outputStream = new FileOutputStream(tempFile)) {
+            byte[] buffer = new byte[8192];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, length);
+            }
+        } finally {
+            inputStream.close();
+        }
+
+        // Load the native library from the temporary file
+        System.load(tempFile.getAbsolutePath());
+
+        Logger.info("Loaded native library: " + library);
     }
 }
